dbce334752e8e54f2ff1a38bbdb3909a7a663072
SSerra007 <samuel.serra@aglets.com.br>
2023-06-23 15:40:12 -0300

    gitignore_prisma

--- src/types/PreviaRetorno.ts
+++ src/types/PreviaRetorno.ts
@@ -2,6 +2,6 @@ import { IRetorno } from "./IRetorno";
 import { listaProduto } from "./ListaProdutosType";
 
 export interface PreviaRetorno extends IRetorno {
-	previa?: listaProduto;
-	idMarketplace?: number
+  previa?: listaProduto;
+  idMarketplace?: number;
 }
